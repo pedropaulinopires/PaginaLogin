4f9e7264945a2462245aed1d0b60118a647cdaa6
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-06-04 23:07:49 -0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,104 @@
+ document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('focus', () => {
+      input.labels.forEach(label => {
+        if(input.value.length == 0){
+            label.classList.add('active')
+        } 
+      })
+    })
+  
+    input.addEventListener('blur', () => {
+      input.labels.forEach(label => {
+        if(input.value.length == 0){
+            label.classList.remove('active')
+        } 
+      })
+    })
+  })
+
+document.getElementById("eyeNot").addEventListener("click",function(){
+    document.getElementById("eyeNot").style.display="none"
+    document.getElementById("eyeOk").style.display="block"
+    document.getElementById("password").setAttribute("type","text")
+})
+
+document.getElementById("eyeOk").addEventListener("click",function(){
+    document.getElementById("eyeOk").style.display="none"
+    document.getElementById("eyeNot").style.display="block"
+    document.getElementById("password").setAttribute("type","password")
+})
+
+document.getElementById("btnSumbmit").addEventListener("click",sumbmit)
+
+function sumbmit(){
+  const password = document.getElementById("password")
+  const username = document.getElementById("username")
+  const label_password = document.getElementById("labelPassword")
+  const label_username = document.getElementById("usernameLabel")
+  const msg_password = document.getElementById("passwordNull")
+  const msg_username = document.getElementById("userNull")
+  if(password.value.length == 0 && username.value.length ==0){
+    msg_password.classList.add("active")
+    msg_username.classList.add("active")
+
+    username.classList.add("error")
+    password.classList.add("error")
+
+    label_password.classList.add("error")
+    label_username.classList.add("error")
+
+  } else if(username.value.length == 0){
+
+    msg_password.classList.remove("active")
+    msg_username.classList.add("active")
+
+    username.classList.add("error")
+    password.classList.remove("error")
+
+    label_password.classList.remove("error")
+    label_username.classList.add("error")
+
+  } else if(password.value.length == 0){
+
+    msg_password.classList.add("active")
+    msg_username.classList.remove("active")
+
+    username.classList.remove("error")
+    password.classList.add("error")
+
+    label_password.classList.add("error")
+    label_username.classList.remove("error")
+
+  } else if(!(password.value == "pedro123") && !(username.value == "pedro")){
+
+    msg_password.classList.add("active")
+    msg_password.innerText="Usuario/senha incorretos!"
+    msg_username.classList.remove("active")
+
+    username.classList.remove("error")
+    password.classList.remove("error")
+
+    label_password.classList.remove("error")
+    label_username.classList.remove("error")
+
+  } else {
+
+    msg_password.classList.remove("active")
+    msg_username.classList.remove("active")
+
+    username.classList.remove("error")
+    password.classList.remove("error")
+
+    label_password.classList.remove("error")
+    label_username.classList.remove("error")
+
+    document.body.style.pointerEvents = "none"
+
+  }
+}
+
+addEventListener("keypress", function(element){
+  if(element.key == "Enter"){
+    sumbmit()
+  }
+})
